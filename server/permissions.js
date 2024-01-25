cd2336c9c4da1b8d394d5d6e8a8ec5f5cf37ad0f
peacekeeper99 <156946000+peacekeeper99@users.noreply.github.com>
2024-01-25 23:08:43 +0000

    Initial commit

---
+++ server/permissions.js
@@ -0,0 +1,42 @@
+module.exports = [
+    {
+        "key": process.env.TOKEN_1,
+        "discordID": "0",
+        "nameColor": "#ffffff",
+        "class": "developer",
+        "infiniteLevelUp": true,
+        "name": "unnamed#0000",
+        "note": "note here",
+        "operator": 0
+    },
+    {
+        "key": process.env.TOKEN_2,
+        "discordID": "0",
+        "nameColor": "#ffffff",
+        "class": "developer",
+        "infiniteLevelUp": true,
+        "name": "unnamed#0000",
+        "note": "note here",
+        "operator": 1
+    },
+    {
+        "key": process.env.TOKEN_3,
+        "discordID": "0",
+        "nameColor": "#ffffff",
+        "class": "developer",
+        "infiniteLevelUp": true,
+        "name": "unnamed#0000",
+        "note": "note here",
+        "operator": 2
+    },
+    {
+        "key": process.env.TOKEN_4,
+        "discordID": "0",
+        "nameColor": "#ffffff",
+        "class": "developer",
+        "infiniteLevelUp": true,
+        "name": "unnamed#0000",
+        "note": "note here",
+        "operator": 3
+    },
+]
